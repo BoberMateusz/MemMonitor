b558f52e6e8b276c48e23c7e01492007efd7534f
Mateusz Bober <mateusz.bober@gmail.com>
2024-09-26 15:59:11 +0200

    Demo version, fully works, readability to be improved

--- MemMonitor/Program.cs
+++ MemMonitor/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
-namespace MemMonitor
+class MemMonitorWriter
 {
-    internal class Program
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string connectionString = ConfigurationManager.AppSettings["connectionString"];
+        var processes = Process.GetProcesses();
+        var timestamp = DateTime.UtcNow;
+        var computerName = Environment.MachineName;
+
+        using (var connection = new SqlConnection(connectionString))
         {
+            connection.Open();
+            using (var transaction = connection.BeginTransaction())
+            {
+                var command = new SqlCommand("INSERT INTO ProcessMemoryUsage (ProcessName, MemoryUsage, Timestamp, ComputerName) VALUES (@ProcessName, @MemoryUsage, @Timestamp, @ComputerName)", connection) { Transaction = transaction };
+                command.Parameters.AddWithValue("@ProcessName", null); // Pre-add parameter for reuse
+                command.Parameters.AddWithValue("@MemoryUsage", 0);  // Pre-add parameter for reuse
+                command.Parameters.AddWithValue("@Timestamp", timestamp);
+                command.Parameters.AddWithValue("@ComputerName", computerName);
+
+                foreach (var process in processes)
+                {
+                    command.Parameters["@ProcessName"].Value = process.ProcessName;
+                    command.Parameters["@MemoryUsage"].Value = process.WorkingSet64;
+                    command.ExecuteNonQuery();
+                    Console.WriteLine($"Inserted: {process.ProcessName}, {process.WorkingSet64}, {timestamp}, {computerName}");
+                }
+
+                transaction.Commit();
+            }
         }
     }
 }
